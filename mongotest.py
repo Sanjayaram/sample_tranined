ba14674e519ce3decf38cc2f28d87fe4e0a0d26e
Sanjayaram <santosh.j033@gmail.com>
2022-07-23 12:05:42 +0530

    new file with function multi

--- mongotest.py
+++ mongotest.py
@@ -3,7 +3,7 @@ client = pymongo.MongoClient("mongodb+srv://santosh:<password>@cluster0.awz3j.mo
 db = client.test
 print(db)
 
-d = {"key":"value"}
-db1 = client['mongotest']
-coll = db1['test']
-coll.insert_one(d)
+#d = {"key":"value"}
+#db1 = client['mongotest']
+#coll = db1['test']
+#coll.insert_one(d)
